afc46f7dbd12aea1dd76d615a573e8ae9b998430
alexanderaltena <37830338+alexanderaltena@users.noreply.github.com>
2021-12-13 18:39:13 +0100

    stay connected ble client

---
+++ ble_client/ble_client_connect.py
@@ -0,0 +1,74 @@
+import asyncio
+
+from bleak import BleakClient, discover
+
+address = (
+     "C0:CC:BB:AA:AA:AA"
+)
+
+devices_dict = {}
+devices_list = []
+receive_data = []
+
+class Connection:
+    
+    client: BleakClient = None
+    
+    def __init__(
+        self,
+        loop: asyncio.AbstractEventLoop,
+    ):
+        self.loop = loop
+        self.connected = False
+       
+    def on_disconnect(self, client: BleakClient, future: asyncio.Future):
+        self.connected = False
+        # Put code here to handle what happens on disconnet.
+        print(f"Disconnected from {self.connected_device.name}!")
+
+    async def cleanup(self):
+        if self.client:
+            await self.client.disconnect()
+
+    async def manager(self):
+        print("Starting connection manager.")
+        while True:
+            if self.client:
+                await self.connect()
+            else:
+                await self.select_device()
+                await asyncio.sleep(10.0)       
+
+    async def connect(self):
+        if self.connected:
+            return
+        try:
+            await self.client.connect()
+            self.connected = await self.client.is_connected()
+            if self.connected:
+                print(F"Connected to {self.connected_device.name}")
+                self.client.set_disconnected_callback(self.on_disconnect)
+                while True:
+                    if not self.connected:
+                        break
+                    await asyncio.sleep(3.0)
+            else:
+                print(f"Failed to connect to {self.connected_device.name}")
+        except Exception as e:
+            print(e)
+
+    async def scan(self):
+        print('scan: before discover')
+        dev = await discover()
+        print('scan: after discover') 
+        for i in range(0,len(dev)):
+            if dev[i].name == "STLB250":
+            #Print the devices discovered
+            #TODO write to the log file
+                print("[" + str(i) + "]" + dev[i].address,dev[i].name,dev[i].metadata["uuids"])
+                devices_dict[dev[i].address] = []
+                devices_dict[dev[i].address].append(dev[i].name)
+                devices_dict[dev[i].address].append(dev[i].metadata["uuids"])
+                devices_list.append(dev[i].address)
+                self.client = BleakClient(dev[i].address, loop=self.loop)
+                
